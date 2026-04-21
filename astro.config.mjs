65e1d79632d0527a79f431a6311a6e9ae6034bb2
tori2k <vladspitsyn@gmail.com>
2026-04-21 14:59:11 +0300

    move to kirillbaryev.ru domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tori2k.github.io',
-  base: '/personal-site-v3',
+  site: 'https://kirillbaryev.ru',
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
